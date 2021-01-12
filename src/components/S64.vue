86b3d8d43a8c790d71feb2973e8562e257037285
tiendatmagic <tiendatmagic8@gmail.com>
2021-01-12 23:09:28 +0700

    update

--- src/components/S64.vue
+++ src/components/S64.vue
@@ -2,55 +2,39 @@
   <div class="smainn">
     <br />
     <div id="s64">
+      <div class="list">
+        <div class="list-group">
+          <ul>
+            <li>
+              <input type="checkbox" name="" id="" />
+              <span> Cá chiên </span>
+            </li>
 
-  <div class="list">
+            <li>
+              <input type="checkbox" name="" id="" />
+              <span> Rau muống </span>
+            </li>
 
+            <li>
+              <input type="checkbox" name="" id="" />
 
-    <div class="list-group">
-      <ul>
-        <li>
-          <input type="checkbox" name="" id="">
-          <span>
-            Cá chiên
-          </span>
-        </li>
+              <span> Tôm hùm </span>
+            </li>
 
-        <li>
-          <input type="checkbox" name="" id="">
-          <span>
-            Rau muống
-          </span>
+            <li>
+              <input type="checkbox" name="" id="" />
 
-        </li>
+              <span> Bánh mì </span>
+            </li>
 
-        <li>
-          <input type="checkbox" name="" id="">
+            <li>
+              <input type="checkbox" name="" id="" />
 
-          <span>
-            Tôm hùm
-          </span>
-        </li>
-
-        <li>
-          <input type="checkbox" name="" id="">
-
-          <span>
-            Bánh mì
-          </span>
-        </li>
-
-        <li>
-          <input type="checkbox" name="" id="">
-
-          <span>
-            Thịt bò
-          </span>
-        </li>
-      </ul>
-    </div>
-  </div>
-
-
+              <span> Thịt bò </span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
 
     <div class="buttonhtmlcss">
@@ -90,89 +74,83 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
- * {
-      margin: 0px;
-      padding: 0px;
-      box-sizing: border-box;
-    }
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
 
-    body {
-      display: flex;
-      height: 100vh;
-      justify-content: center;
-      align-items: center;
-      overflow-y: auto;
-    }
+body {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  overflow-y: auto;
+}
 
-    .list-group::-webkit-scrollbar {
-      width: 7px;
-    }
+.list-group::-webkit-scrollbar {
+  width: 7px;
+}
 
-    .list-group::-webkit-scrollbar-thumb {
-      background: #6D7696;
-      border-radius: 30px;
-    }
+.list-group::-webkit-scrollbar-thumb {
+  background: #6d7696;
+  border-radius: 30px;
+}
 
-    .list-group::-webkit-scrollbar-thumb:hover {
-      background: #232f5e;
-    }
+.list-group::-webkit-scrollbar-thumb:hover {
+  background: #232f5e;
+}
 
-    .list {
-      width: 250px;
-      padding: 10px;
-      border-radius: 10px;
-      margin: 0 auto;
-      background-color: #353F66;
-    }
+.list {
+  width: 250px;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 0 auto;
+  background-color: #353f66;
+}
 
+.list-group {
+  height: 150px;
+  overflow-y: auto;
+}
 
+.list-group ul {
+  list-style: none;
+}
 
-    .list-group {
+.list-group li {
+  margin: 15px;
+  font-size: 20px;
+  color: #fff;
+  display: flex;
+  align-items: center;
+}
 
-      height: 150px;
-      overflow-y: auto;
-    }
+.list-group li input {
+  width: 15px;
+  height: 15px;
+  background-color: #fff;
+  position: relative;
+}
 
-    .list-group ul {
-      list-style: none;
-    }
+.list-group li input::after {
+  content: "";
+  width: 20px;
+  height: 20px;
+  top: -4px;
+  left: -2.5px;
+  position: relative;
+  border: 1px solid #fff;
+  border-radius: 8px;
+  background-color: #353f66;
+  display: inline-block;
+}
 
-    .list-group li {
-      margin: 15px;
-      font-size: 20px;
-      color: #fff;
-      display: flex;
-      align-items: center;
+.list-group li input:checked:after {
+  background-color: #aeb2c2;
+}
 
-    }
-
-    .list-group li input {
-      width: 15px;
-      height: 15px;
-      background-color: #fff;
-      position: relative;
-    }
-
-    .list-group li input::after {
-      content: "";
-      width: 20px;
-      height: 20px; 
-      top: -4px;
-      left: -2.5px;
-      position: relative;
-      border: 1px solid #fff;
-      border-radius: 8px;
-      background-color: #353F66;
-      display: inline-block;
-
-    }
-
-    .list-group li input:checked:after 
-    {
-      background-color: #AEB2C2;
-    }
-
-    .list-group li span {
-      margin-left: 20px;
-    }
+.list-group li span {
+  margin-left: 20px;
+}
 </style>
