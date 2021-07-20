146dd54b17176fc9f30ac10cd1ed9c8d3a0b8fd0
tiendatmagic <tiendatmagic8@gmail.com>
2021-07-20 12:40:13 +0700

    update

--- src/components/S15.vue
+++ src/components/S15.vue
@@ -2,8 +2,7 @@
   <div class="smainn">
     <br />
     <div id="s15">
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
+      <link rel="preconnect" href="https://fonts.gstatic.com" /><link
         href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap"
         rel="stylesheet"
       />
@@ -17,19 +16,12 @@
           />
         </div>
         <p>Connect</p>
-
         <div class="number">
-          <span>1234</span>
-          <span>5678</span>
-          <span>9012</span>
-          <span>3456</span>
+          <span>1234</span><span>5678</span><span>9012</span><span>3456</span>
         </div>
-
         <p class="info">
-          <span>MEMBERSINCE:</span>
-          <span>01/1-2020</span>
-          <span>CLASS</span>
-          <span> A</span>
+          <span>MEMBERSINCE:</span><span>01/20</span><span>CLASS:</span
+          ><span> A</span>
         </p>
         <p>CARRD HOLDER</p>
       </div>
@@ -73,10 +65,15 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
-
+body {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .card-atm {
   width: 100%;
-  max-width: 430px;
+  max-width: 400px;
   height: 250px;
   margin: 0 auto;
   border-radius: 20px;
@@ -85,25 +82,9 @@ export default {
   background-position-x: -100px;
   background-position-y: -220px;
   background-repeat: no-repeat;
-  background-color: rgb(255, 255, 255);
+  background-color: white;
   box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.75);
-}
-
-.card-atm p:nth-child(1) {
-  color: rgb(0, 70, 35);
-  font-weight: 1000;
-  text-align: right;
-  margin-top: 30px;
-
-  font-size: 24px;
-}
-
-.card-atm p:nth-child(2) {
-  color: rgb(0, 70, 35);
-  font-weight: bold;
-  text-align: right;
-  margin-right: 100px;
-  font-size: 24px;
+  overflow: hidden;
 }
 
 .cardlogo {
@@ -111,7 +92,6 @@ export default {
   justify-content: flex-end;
   margin-right: 20px;
 }
-
 .cardlogo img {
   display: flex;
   width: 40px;
@@ -122,39 +102,50 @@ export default {
   background-color: #fff;
 }
 
+.cardlogo p:nth-child(1) {
+  color: #004623;
+  font-weight: 1000;
+  text-align: right;
+  margin-top: 30px;
+  font-size: 24px;
+}
+.card-atm p:nth-child(2) {
+  color: #004623;
+  font-weight: bold;
+  text-align: right;
+  margin-right: 100px;
+  font-size: 24px;
+}
 .number {
   display: flex;
   justify-content: space-around;
   font-family: "Orbitron", sans-serif;
   margin-top: 30px;
 }
-
 p.info {
   margin-top: 30px;
   text-align: center;
 }
-
+p.info span {
+  margin: 0 5px;
+}
 p.info span:nth-child(1) {
   color: #000;
   font-size: 12px;
 }
-
 p.info span:nth-child(2) {
   color: #fff;
   font-size: 19px;
   font-family: "Orbitron", sans-serif;
 }
-
 p.info span:nth-child(3) {
   color: #000;
   font-size: 12px;
 }
-
 p.info span:nth-child(4) {
   color: #fff;
   font-size: 19px;
 }
-
 p:nth-child(5) {
   color: #fff;
   text-align: center;
